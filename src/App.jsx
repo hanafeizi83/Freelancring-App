9a7f8909719a38772f222bb6e2bd258dded54f7b
hana feizi <hanafaizi83@gmail.com>
2025-03-22 10:04:50 +0330

    install and add react router dom to projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Home from './pages/Home'
+import Auth from './pages/Auth'
 
 function App() {
   return (
-    <>
-      
-    </>
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/auth' element={<Auth />} />
+    </Routes>
   )
 }
 
