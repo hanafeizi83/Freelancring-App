ebf445cc9e6e7b8d739d29c2f860b17b9a17f629
hana feizi <hanafaizi83@gmail.com>
2025-03-22 10:18:33 +0330

    change defualt extend tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,56 @@
 /** @type {import('tailwindcss').Config} */
+import { fontFamily } from 'tailwindcss/defaultTheme'
+
+function colorWithOpacity(variableName) {
+  return ({ opacityValue }) => {
+    if (opacityValue !== undefined) {
+      return `rgba(var(${variableName}),${opacityValue})`
+    }
+    return `rgb(var(${variableName}))`
+  }
+}
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true
+      },
+      colors: {
+        primary: {
+          100: colorWithOpacity('--color-primary-100'),
+          200: colorWithOpacity('--color-primary-200'),
+          300: colorWithOpacity('--color-primary-300'),
+          400: colorWithOpacity('--color-primary-400'),
+          500: colorWithOpacity('--color-primary-500'),
+          600: colorWithOpacity('--color-primary-600'),
+          700: colorWithOpacity('--color-primary-700'),
+          800: colorWithOpacity('--color-primary-800'),
+          900: colorWithOpacity('--color-primary-900')
+        },
+        secondary: {
+          100: colorWithOpacity('--color-secondary-100'),
+          200: colorWithOpacity('--color-secondary-200'),
+          300: colorWithOpacity('--color-secondary-300'),
+          400: colorWithOpacity('--color-secondary-400'),
+          500: colorWithOpacity('--color-secondary-500'),
+          600: colorWithOpacity('--color-secondary-600'),
+          700: colorWithOpacity('--color-secondary-700'),
+          800: colorWithOpacity('--color-secondary-800'),
+          900: colorWithOpacity('--color-secondary-900')
+        },
+        error: colorWithOpacity('--color-error'),
+        warning: colorWithOpacity('--color-warning'),
+        success: colorWithOpacity('--color-success')
+      },
+      fontFamily: {
+        sans: ['Vazir', ...fontFamily.sans]
+      }
+    },
   },
   plugins: [],
 }
