cd4fb964fa07dc1131275fe0aecfd5b98c152966
hana feizi <hanafaizi83@gmail.com>
2025-04-02 00:07:35 +0330

    make handle error 401

--- src/services/httpServices.js
+++ src/services/httpServices.js
@@ -1,10 +1,34 @@
 import axios from "axios";
+
 const BASE_URL = 'http://localhost:5000/api';
 
 const app = axios.create({
     baseURL: BASE_URL,
     withCredentials: true
 });
+
+app.interceptors.request.use(
+    res => res,
+    err => Promise.reject(err)
+)
+
+app.interceptors.response.use(
+    res => res,
+    err => {
+        const OriginalConfig = err.config;
+        if (err.response.status == 401 && !OriginalConfig._retry) {
+            OriginalConfig._retry = true;
+            try {
+                const { data } = axios.get(`${BASE_URL}/user/refresh-token`, { withCredentials: true });
+                if (data) return app(OriginalConfig);
+            } catch (error) {
+                return Promise.reject(error);
+            }
+        }
+        return Promise.reject(err)
+    }
+)
+
 
 const http = {
     get: app.get,
