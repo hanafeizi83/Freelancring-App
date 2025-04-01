56acbe102ed998272636601c0c0b10ed7721fa46
hana feizi <hanafaizi83@gmail.com>
2025-04-01 09:53:17 +0330

    make admin  dashboard

--- src/features/admin/Stats.jsx
+++ src/features/admin/Stats.jsx
@@ -1,31 +1,31 @@
 import React from 'react'
-import State from '../../ui/State'
-import useProjects from './../freelancer/project/useProjects'
-import useProposals from './../proposals/useProposals'
-import { toPersianNumbersWithComma } from '../../utils/toPresionNumbers';
+import State from './../../ui/State'
+import useProjects from '../freelancer/project/useProjects';
+import useProposals from '../proposals/useProposals';
+import useUsers from './users/useUsers'
+
 function Stats() {
     const { projects = [] } = useProjects();
     const { proposals = [] } = useProposals();
-    const wallet = proposals.reduce((acc, curr) => curr.price + acc, 0)
-
+    const { users = [] } = useUsers();
     return (
         <div className='grid mt-4 grid-cols-1 grid-rows-3 gap-5 sm:grid-cols-2 sm:grid-rows-2 lg:grid-cols-3 lg:grid-rows-1 '>
+            <State
+                label='کاربران'
+                value={users.length}
+                img='users.png'
+                color='purple'
+            />
             <State
                 label='پروژه ها'
                 value={projects.length}
                 img='projects.png'
-                color='purple'
+                color='green'
             />
             <State
                 label='درخواست ها'
                 value={proposals.length}
                 img='project success.png'
-                color='green'
-            />
-            <State
-                label='کیف پول'
-                value={toPersianNumbersWithComma(wallet)}
-                img='wallet.png'
                 color='pink'
             />
         </div>
--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import DashboardLayout from '../features/admin/DashboardLayout'
 
 function AdminDashboard() {
   return (
     <div>
-      
+      <DashboardLayout />
     </div>
   )
 }
